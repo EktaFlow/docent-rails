058d228ccd4f0c5bb3c1bc1daedf7e6de7480853
Dominique Goncalves <dominique@ekta.co>
2022-04-04 09:52:15 -0500

    finishing connection between fe and be for create new assessment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,8 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:question_id])
+    render json: {question: @question.get_info, subthread: @question.subthread, thread: @question.subthread.mr_thread}
   end
 
   def create
