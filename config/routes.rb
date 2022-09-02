0635724fec533eb62b90dec9b8e1b5f34125abe1
Dominique Goncalves <dominique@ekta.co>
2022-09-02 08:55:09 -0500

    fixed handful of issues

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
                  registrations: 'users/registrations'
              }
 
+  root to: 'assessments#index'
 
   # get '/browse', to: 'projects#browse'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
